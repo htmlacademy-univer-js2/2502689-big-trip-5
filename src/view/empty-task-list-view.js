5846397b3256e5881c5be060f7b1dee07bea080b
Elena <Sukhorosova.Lena@yandex.ru>
2025-06-12 12:11:05 +0500

    module4-task2

---
+++ src/view/empty-task-list-view.js
@@ -0,0 +1,12 @@
+import AbstractView from '../framework/view/abstract-view.js';
+
+
+function createEmptyPointListTemplate() {
+  return '<p class="trip-events__msg">Click New Event to create your first point</p>';
+}
+
+export default class EmptyPointList extends AbstractView{
+  get template() {
+    return createEmptyPointListTemplate();
+  }
+}
